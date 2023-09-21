1d268b5d41899bcedf209aec932f3a340c843f00
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-21 14:44:12 +0900

    add:prac2

---
+++ Question_01_10/practices_py/prac_3.py
@@ -0,0 +1,23 @@
+import cv2 
+
+def RGB2BGR(img):
+  b = img[:,:,0].copy()
+  g = img[:,:,1].copy()
+  r = img[:,:,2].copy()
+  
+  # RGB > BGR
+  img[:,:,0] = r
+  img[:,:,1] = g
+  img[:,:,2] = b
+  
+  return img
+
+#read_img
+img = cv2.imread('imori.jpg')
+img2 = RGB2BGR(img)
+
+
+cv2.imwrite('answers_image/answer1.jpg',img2)
+cv2.imshow('',img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
