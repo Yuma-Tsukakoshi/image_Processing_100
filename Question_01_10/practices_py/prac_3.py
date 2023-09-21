4d1195723fa628353f150c25402388cf82c35455
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-21 15:01:28 +0900

    add:prac3

--- Question_01_10/practices_py/prac_3.py
+++ Question_01_10/practices_py/prac_3.py
@@ -1,23 +1,27 @@
 import cv2 
+import numpy as np
 
-def RGB2BGR(img):
+def BGR2GRAY(img):
   b = img[:,:,0].copy()
   g = img[:,:,1].copy()
   r = img[:,:,2].copy()
-  
-  # RGB > BGR
-  img[:,:,0] = r
-  img[:,:,1] = g
-  img[:,:,2] = b
-  
+
+  out = 0.2126*r + 0.7152*g + 0.0722*b
+  out = out.astype(np.uint8)  
+  return out
+
+def binary_scale(img, th=128):
+  img[img < th] = 0
+  img[img >= th] = 255
   return img
 
 #read_img
 img = cv2.imread('imori.jpg')
-img2 = RGB2BGR(img)
+img2 = BGR2GRAY(img)
+img2 = binary_scale(img2)
 
-
-cv2.imwrite('answers_image/answer1.jpg',img2)
+#result_img
+cv2.imwrite('answers_image/answer3.jpg',img2)
 cv2.imshow('',img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
