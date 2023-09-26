215ba8c9eae722f687b2e2485e5eb110aa95df2c
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-26 10:04:47 +0900

    prac12

--- Question_11_20/practices_py/prac_13.py
+++ Question_11_20/practices_py/prac_13.py
@@ -1,27 +1,41 @@
-import cv2 
+import cv2
 import numpy as np
 
-def BGR2GRAY(img):
-  b = img[:,:,0].copy()
-  g = img[:,:,1].copy()
-  r = img[:,:,2].copy()
+def gaussian(img, K_size=3, sigma=1.3):
+  
+  H, W, C = img.shape
 
-  out = 0.2126*r + 0.7152*g + 0.0722*b
-  out = out.astype(np.uint8)  
+  ## Zero padding
+  pad = K_size // 2
+  out = np.zeros((H + pad * 2, W + pad * 2, C), dtype=np.float) #上下左右に1pxずつ0でpadding⇒×2
+  out[pad: pad + H, pad: pad + W] = img.copy().astype(np.float)  
+  
+  ## prepare Kernel
+  K = np.zeros((K_size, K_size), dtype=np.float)
+  for x in range(-pad, -pad + K_size):
+    for y in range(-pad, -pad + K_size):
+      K[y + pad, x + pad] = np.exp( -(x ** 2 + y ** 2) / (2 * (sigma ** 2)))
+      
+  K /= (2 * np.pi * sigma * sigma)
+  K /= K.sum()  
+  tmp = out.copy()  
+  
+  # filtering
+  for y in range(H):
+    for x in range(W):
+      for c in range(C):
+        out[pad + y, pad + x, c] = np.sum(K * tmp[y: y + K_size, x: x + K_size, c])  
+
+  out = np.clip(out, 0, 255)
+  out = out[pad: pad + H, pad: pad + W].astype(np.uint8)  
   return out
 
-def binary_scale(img, th=128):
-  img[img < th] = 0
-  img[img >= th] = 255
-  return img
-
 #read_img
-img = cv2.imread('Question_01_10\imori.jpg')
-img2 = BGR2GRAY(img)
-img2 = binary_scale(img2)
+img = cv2.imread('Question_01_10\imori_noise.jpg')
+out = gaussian(img, K_size=3, sigma=1.3)
 
 #result_img
-cv2.imwrite('answers_image/answer3.jpg',img2)
-cv2.imshow('',img2)
+# cv2.imwrite('answers_image/answer4.jpg',img2)
+cv2.imshow('result',out)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
