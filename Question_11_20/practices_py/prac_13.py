a3fc3a008a9db0f14a8cf019e51637031423a3e6
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-26 10:15:53 +0900

    add:prac13

--- Question_11_20/practices_py/prac_13.py
+++ Question_11_20/practices_py/prac_13.py
@@ -1,38 +1,39 @@
 import cv2
 import numpy as np
 
-def gaussian(img, K_size=3, sigma=1.3):
+def BGR2GRAY(img):
+  b = img[:,:,0].copy()
+  g = img[:,:,1].copy()
+  r = img[:,:,2].copy()
+
+  out = 0.2126*r + 0.7152*g + 0.0722*b
+  out = out.astype(np.uint8)  
+  return out
+
+def max_min(img, K_size=3):
   
-  H, W, C = img.shape
+  H, W= img.shape
 
   ## Zero padding
   pad = K_size // 2
-  out = np.zeros((H + pad * 2, W + pad * 2, C), dtype=np.float) #上下左右に1pxずつ0でpadding⇒×2
+  out = np.zeros((H + pad * 2, W + pad * 2), dtype=np.float) #上下左右に1pxずつ0でpadding⇒×2
   out[pad: pad + H, pad: pad + W] = img.copy().astype(np.float)  
   
-  ## prepare Kernel
-  K = np.zeros((K_size, K_size), dtype=np.float)
-  for x in range(-pad, -pad + K_size):
-    for y in range(-pad, -pad + K_size):
-      K[y + pad, x + pad] = np.exp( -(x ** 2 + y ** 2) / (2 * (sigma ** 2)))
-      
-  K /= (2 * np.pi * sigma * sigma)
-  K /= K.sum()  
   tmp = out.copy()  
   
   # filtering
   for y in range(H):
     for x in range(W):
-      for c in range(C):
-        out[pad + y, pad + x, c] = np.sum(K * tmp[y: y + K_size, x: x + K_size, c])  
+      out[pad + y, pad + x] = np.max(tmp[y: y + K_size, x: x + K_size ]) - np.min(tmp[y: y + K_size, x: x + K_size])  
 
   out = np.clip(out, 0, 255)
   out = out[pad: pad + H, pad: pad + W].astype(np.uint8)  
   return out
 
 #read_img
-img = cv2.imread('Question_01_10\imori_noise.jpg')
-out = gaussian(img, K_size=3, sigma=1.3)
+img = cv2.imread('Question_01_10\imori.jpg')
+out = BGR2GRAY(img)
+out = max_min(out, K_size=3)
 
 #result_img
 # cv2.imwrite('answers_image/answer4.jpg',img2)
