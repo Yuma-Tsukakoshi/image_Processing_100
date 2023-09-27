105741c27163a67d5b4e77c256dcc88ed9e4cd00
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-27 09:35:37 +0900

    add:prac14

--- Question_11_20/practices_py/prac_14.py
+++ Question_11_20/practices_py/prac_14.py
@@ -1,4 +1,4 @@
-import cv2 
+import cv2
 import numpy as np
 
 def BGR2GRAY(img):
@@ -10,41 +10,39 @@ def BGR2GRAY(img):
   out = out.astype(np.uint8)  
   return out
 
-# 式の導出からSbを最大にする閾値を求める
-def otsu_binary_scale(img, th=128):
-  max_th = 0
-  max_Sb = 0
+def sobel_filter(img, K_size=3):
+  
+  H, W= img.shape
 
-  for _t in range(1,255):
-    # v0とv1の値が結構重要なポイントとなる
-    # 閾値となりうるのが0~255となり一つ一つ試す
-    v0 = img[np.where(img < _t)]
-    v1 = img[np.where(img >= _t)]
-    # ↑ np.whereによって条件に合う要素のインデックスを返し、それをimgに適用することで条件に合う要素を返す
-    
-    m0 = np.mean(v0) 
-    m1 = np.mean(v1)
-    w0 = len(v0) / H*W
-    w1 = len(v1) / H*W
-    Sb = w0*w1*((m0-m1)**2)  
-    if Sb > max_Sb:
-      max_Sb = Sb
-      max_th = _t
+  ## Zero padding
+  pad = K_size // 2
+  out = np.zeros((H + pad * 2, W + pad * 2), dtype=np.float) #上下左右に1pxずつ0でpadding⇒×2
+  out[pad: pad + H, pad: pad + W] = img.copy().astype(np.float)  
+  
+  ## prepare Kernel
+  K = np.array([[1., 2., 1.],[0., 0., 0.],[-1., -2., -1.]])
+  # K= np.array([[1., 0., -1.],[2., 0., -2.],[1., 0., -1.]])
 
-  #二値化
-  print("threshold: ",max_th)
-  th = max_th
-  out[out < th] = 0
-  out[out >= th] = 255
+  
+  tmp = out.copy()  
+  
+  # filtering
+  for y in range(H):
+    for x in range(W):
+      out[pad + y, pad + x] = np.sum(K*tmp[y: y + K_size, x: x + K_size ])
+
+  # clip操作で0~255に収める操作結構重要
+  out = np.clip(out, 0, 255)
+  out = out[pad: pad + H, pad: pad + W].astype(np.uint8)  
   return out
 
 #read_img
-img = cv2.imread('Question_01_10\imori.jpg').astype(np.float32)
-H,W,C = img.shape
+img = cv2.imread('Question_01_10\imori.jpg')
 out = BGR2GRAY(img)
-out = otsu_binary_scale(out)
+out = sobel_filter(out, K_size=3)
 
 #result_img
-cv2.imshow('',out)
+# cv2.imwrite('answers_image/answer4.jpg',img2)
+cv2.imshow('result',out)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
