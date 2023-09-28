07048137661d54e42f1e748cd3a2273c89a9233f
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-28 10:02:34 +0900

    add:prac16

--- Question_11_20/practices_py/prac_16.py
+++ Question_11_20/practices_py/prac_16.py
@@ -1,20 +1,45 @@
-import cv2 
+import cv2
 import numpy as np
 
-def reduce_color(_img):
-  img = _img.copy()
+def BGR2GRAY(img):
+  b = img[:,:,0].copy()
+  g = img[:,:,1].copy()
+  r = img[:,:,2].copy()
+
+  out = 0.2126*r + 0.7152*g + 0.0722*b
+  out = out.astype(np.uint8)  
+  return out
+
+def prewitt_filter(img, K_size=3):
   
-  for i in range(4):
-    for j in range(3):
-      val = img[...,j]  
-      ind = np.where((i * (4**3) <= val ) & (val < (i+1) * (4**3)))
-      img[...,j][ind] = 32 + i * 4**3 
+  H, W= img.shape
+
+  ## Zero padding
+  pad = K_size // 2
+  out = np.zeros((H + pad * 2, W + pad * 2), dtype=np.float) #上下左右に1pxずつ0でpadding⇒×2
+  out[pad: pad + H, pad: pad + W] = img.copy().astype(np.float)  
   
-  return img
+  ## prepare Kernel
+  # K = np.array([[-1., -1., -1.],[0., 0., 0.],[1., 1., 1.]])
+  K= np.array([[-1., 0., 1.],[-1., 0., 1.],[-1., 0., 1.]])
+
+  
+  tmp = out.copy()  
+  
+  # filtering
+  for y in range(H):
+    for x in range(W):
+      out[pad + y, pad + x] = np.sum(K*tmp[y: y + K_size, x: x + K_size ])
+
+  # clip操作で0~255に収める操作結構重要
+  out = np.clip(out, 0, 255)
+  out = out[pad: pad + H, pad: pad + W].astype(np.uint8)  
+  return out
 
 #read_img
 img = cv2.imread('Question_01_10\imori.jpg')
-out = reduce_color(img)
+out = BGR2GRAY(img)
+out = prewitt_filter(out, K_size=3)
 
 #result_img
 # cv2.imwrite('answers_image/answer4.jpg',img2)
