e2a047c48875eb9cd2c6eb931b2956dc7643bf0c
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-28 11:38:15 +0900

    add:prac20 matplotlib使う時はターミナルから

--- Question_11_20/practices_py/prac_20.py
+++ Question_11_20/practices_py/prac_20.py
@@ -1,34 +1,28 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+# matplotlibを使う場合はターミナル上から実行すること！！
 
-def gaussian(img, K_size=3, sigma=1.3):
-  
-  H, W, C = img.shape
+# Read image
+img = cv2.imread("Question_11_20\imori_dark.jpg").astype(np.float)
 
-  ## Zero padding
-  pad = K_size // 2
-  out = np.zeros((H + pad * 2, W + pad * 2, C), dtype=np.float) #上下左右に1pxずつ0でpadding⇒×2
-  out[pad: pad + H, pad: pad + W] = img.copy().astype(np.float)  
+# Display histogram
+plt.hist(img.ravel(), bins=255, rwidth=0.8, range=(0, 255))
+# plt.savefig("out.png")
+plt.show()
 
-  # 更新時に常に元の画像を参照するようにする
-  tmp = out.copy()  
-  
-  # filtering
-  for y in range(H):
-    for x in range(W):
-      for c in range(C):
-        out[pad + y, pad + x, c] = np.median(tmp[y: y + K_size, x: x + K_size, c])  
+# my_ans 間違い！
+# def show_graph(img):
+#   H, W, C = img.shape
+#   data = np.zeros((256))
+#   for y in range(H):
+#     for x in range(W):
+#       for c in range(C):
+#         data[img[y,x,c]] += 1
+#   return data
 
-  out = out[pad: pad + H, pad: pad + W].astype(np.uint8)  
-  return out
-
-#read_img
-img = cv2.imread('Question_01_10\imori_noise.jpg')
-out = gaussian(img, K_size=3, sigma=1.3)
-
-#result_img
-# cv2.imwrite('answers_image/answer4.jpg',img2)
-cv2.imshow('result',out)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# img = cv2.imread('Question_11_20\imori_dark.jpg')  
+# out = show_graph(img)
+# plt.hist(out, bins=255, rwidth=0.8)
+# plt.show()
 
