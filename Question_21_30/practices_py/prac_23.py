badf601bed52ea0c285c570aaa29ec162ea5e6f3
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-01 00:59:43 +0900

    add:prac23 matplotlib系終了

--- Question_21_30/practices_py/prac_23.py
+++ Question_21_30/practices_py/prac_23.py
@@ -3,33 +3,36 @@
 
 import cv2
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # Read image
-img = cv2.imread("Question_21_30\imori_dark.jpg")
+img = cv2.imread("Question_21_30\imori.jpg")
 
 # histogram normalization
-def hist_normalization(img, a=0, b=255):
-	# get max and min
-	c = img.min()
-	d = img.max()
-
-	out = img.copy()
-
-	# normalization
-	out = (b-a) / (d - c) * (out - c) + a
-	out[out < a] = a
-	out[out > b] = b
-	out = out.astype(np.uint8)
-	
-	return out
+def hist_normalization(img, z_max = 255):
+  out = img.copy()
+  H,W,C = out.shape
+  
+  S = H* W * C  
+  
+  sum_h = 0
+  
+  # np.whereで条件に合う要素のインデックスを返してその長さを取得することで頻度を求めることができる上手い！
+  for i in range(1,255):
+    ind = np.where(img == i)
+    sum_h += len(img[ind])
+    z_prime = z_max / S * sum_h
+    out[ind] = z_prime
+  
+  out = out.astype(np.uint8)
+  return out
 
 # histogram normalization
 out = hist_normalization(img)
 
 # Display histogram
-# plt.hist(out.ravel(), bins=255, rwidth=0.8, range=(0, 255))
-# plt.show()
+plt.hist(out.ravel(), bins=255, rwidth=0.8, range=(0, 255))
+plt.show()
 
 # Save result
 cv2.imshow("result", out)
