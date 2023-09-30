eb89af8c3947efefa55a686abe87564cfed8490a
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-01 00:43:22 +0900

    add:prac22

--- Question_21_30/practices_py/prac_22.py
+++ Question_21_30/practices_py/prac_22.py
@@ -1,48 +1,31 @@
+# matlplotlib はgooglecolobで用いて使うことにする
+# 画像のimg.showはvscodeでやる
+
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def gaussian(img, K_size=3, sigma=1.3):
-  
-  H, W, C = img.shape
+# Read image
+img = cv2.imread("Question_21_30\imori_dark.jpg")
 
-  ## Zero padding
-  pad = K_size // 2
-  out = np.zeros((H + pad * 2, W + pad * 2, C), dtype=np.float) #上下左右に1pxずつ0でpadding⇒×2
-  out[pad: pad + H, pad: pad + W] = img.copy().astype(np.float)  
+# histogram normalization
+def hist_normalization(img, m0=128, s0=52):
+  out = img.copy()
   
-  # answer ⇒ diagで対角行列を作成
-  # Kernel
-  K = np.diag( [1] * K_size ).astype(np.float)
-  K /= K_size
+  m = np.mean(out)
+  s = np.std(out)
   
-  # my_ans
-  ## prepare Kernel
-  # K = np.zeros((K_size, K_size), dtype=np.float)
-  # for x in range(-pad, -pad + K_size):
-  #   for y in range(-pad, -pad + K_size):
-  #     if x==y:
-  #       K[y + pad, x + pad] = 1/3
-  #     else:
-  #       K[y + pad, x + pad] = 0
-      
-  tmp = out.copy()  
-  
-  # filtering
-  for y in range(H):
-    for x in range(W):
-      for c in range(C):
-        out[pad + y, pad + x, c] = np.sum(K * tmp[y: y + K_size, x: x + K_size, c])  
-  
-
-  out = out[pad: pad + H, pad: pad + W].astype(np.uint8)  
+  out = s0 * (out - m ) / s + m0
+  out = out.astype(np.uint8)
   return out
 
-#read_img
-img = cv2.imread('Question_11_20\imori.jpg')
-out = gaussian(img, K_size=3, sigma=1.3)
+# histogram normalization
+out = hist_normalization(img)
 
-#result_img
-# cv2.imwrite('answers_image/answer4.jpg',img2)
-cv2.imshow('result',out)
+# Display histogram
+plt.hist(out.ravel(), bins=255, rwidth=0.8, range=(0, 255))
+plt.show()
+
+# Save result
+cv2.imshow("result", out)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
--- Question_21_30/practices_py/prac_23.py
+++ Question_21_30/practices_py/prac_23.py
@@ -1,42 +1,36 @@
+# matlplotlib はgooglecolobで用いて使うことにする
+# 画像のimg.showはvscodeでやる
+
 import cv2
 import numpy as np
+# import matplotlib.pyplot as plt
 
-def BGR2GRAY(img):
-  b = img[:,:,0].copy()
-  g = img[:,:,1].copy()
-  r = img[:,:,2].copy()
+# Read image
+img = cv2.imread("Question_21_30\imori_dark.jpg")
 
-  out = 0.2126*r + 0.7152*g + 0.0722*b
-  out = out.astype(np.uint8)  
-  return out
+# histogram normalization
+def hist_normalization(img, a=0, b=255):
+	# get max and min
+	c = img.min()
+	d = img.max()
 
-def max_min(img, K_size=3):
-  
-  H, W= img.shape
+	out = img.copy()
 
-  ## Zero padding
-  pad = K_size // 2
-  out = np.zeros((H + pad * 2, W + pad * 2), dtype=np.float) #上下左右に1pxずつ0でpadding⇒×2
-  out[pad: pad + H, pad: pad + W] = img.copy().astype(np.float)  
-  
-  tmp = out.copy()  
-  
-  # filtering
-  for y in range(H):
-    for x in range(W):
-      out[pad + y, pad + x] = np.max(tmp[y: y + K_size, x: x + K_size ]) - np.min(tmp[y: y + K_size, x: x + K_size])  
+	# normalization
+	out = (b-a) / (d - c) * (out - c) + a
+	out[out < a] = a
+	out[out > b] = b
+	out = out.astype(np.uint8)
+	
+	return out
 
-  out = np.clip(out, 0, 255)
-  out = out[pad: pad + H, pad: pad + W].astype(np.uint8)  
-  return out
+# histogram normalization
+out = hist_normalization(img)
 
-#read_img
-img = cv2.imread('Question_01_10\imori.jpg')
-out = BGR2GRAY(img)
-out = max_min(out, K_size=3)
+# Display histogram
+# plt.hist(out.ravel(), bins=255, rwidth=0.8, range=(0, 255))
+# plt.show()
 
-#result_img
-# cv2.imwrite('answers_image/answer4.jpg',img2)
-cv2.imshow('result',out)
+# Save result
+cv2.imshow("result", out)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
