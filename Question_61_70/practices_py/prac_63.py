22e930c2bbdea7934689e4c54a0a820e273b1c52
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-27 20:21:52 +0900

    細線化

--- Question_61_70/practices_py/prac_63.py
+++ Question_61_70/practices_py/prac_63.py
@@ -1,16 +1,19 @@
 import cv2
 import numpy as np
+from skimage.morphology import skeletonize
 
 # Read image
-img = cv2.imread("Question_51_60\imori.jpg")
+img = cv2.imread("Question_61_70\gazo.png")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # 大津の手法
 ret, bin_img = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
-out = cv2.morphologyEx(bin_img, cv2.MORPH_BLACKHAT, kernel, iterations=3)
+#細線化(スケルトン化)
+ske = skeletonize(~(img != 0))
+ske_gray = (ske * 255).astype(np.uint8)
+dst = cv2.cvtColor(ske_gray, cv2.COLOR_GRAY2RGB)
 
 # Save result
-cv2.imshow("result", out)
+cv2.imshow("result", dst)
 cv2.waitKey(0)
