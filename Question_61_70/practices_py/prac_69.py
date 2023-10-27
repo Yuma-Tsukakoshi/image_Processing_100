d2a6f5e50b40865f8f9cfe0c51b72f54ca10cacd
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-27 20:11:18 +0900

    4連結数 よくわからない

---
+++ Question_61_70/practices_py/prac_69.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read image
+img = cv2.imread("Question_51_60\seg.png")
+# グレースケールに変換する。
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 2値化する
+ret, bin_img = cv2.threshold(gray, 20, 255, cv2.THRESH_BINARY)
+
+n_labels, out = cv2.connectedComponents(bin_img,connectivity=8)
+
+# Save result
+print("number of labels:", n_labels)
+fig, ax = plt.subplots(figsize=(7, 7))
+ax.imshow(out)
+cv2.waitKey(0)
+
