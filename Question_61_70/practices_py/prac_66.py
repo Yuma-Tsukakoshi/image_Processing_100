516d05915167460d9686d58ff5b48115eb38d298
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-28 16:27:02 +0900

    add: prac66

--- Question_61_70/practices_py/prac_66.py
+++ Question_61_70/practices_py/prac_66.py
@@ -16,13 +16,42 @@ def hog(img):
     mag = np.sqrt(gx**2 + gy**2) / 255.
     ang = np.arctan(gy / gx)
     
+    ang[ang < 0] = np.pi / 2 + ang[ang < 0] + np.pi / 2
+    
     # quantization 量子化する
     ang_quantized = np.zeros_like(ang, dtype=np.int)
     
+    # quantization base
+    d = np.pi / 9
+
+    # quantization
+    for i in range(9):
+        ang_quantized[np.where((ang >= d * i) & (ang <= d * (i + 1)))] = i
     
+    return mag, ang_quantized
+
+mag , ang_quantized = hog(img)
+
+# Write gradient magnitude to file
+_mag = (mag / mag.max() * 255).astype(np.uint8)
+
+cv2.imshow("out_mag.jpg", _mag)
+
+# Write gradient angle to file
+H, W, C = img.shape
+out = np.zeros((H, W, 3), dtype=np.uint8)
+
+# define color
+C = [[255, 0, 0], [0, 255, 0], [0, 0, 255], [255, 255, 0], [255, 0, 255], [0, 255, 255],
+    [127, 127, 0], [127, 0, 127], [0, 127, 127]]
+
+# draw color
+for i in range(9):
+    out[ang_quantized == i] = C[i]
+
     
 #result
-cv2.imshow('result', )
+cv2.imshow('result', out)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
