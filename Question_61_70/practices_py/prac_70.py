acf5f80321b4f76abaf448db6f83644abe1c9c59
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-28 17:19:48 +0900

    add:prac70

--- Question_61_70/practices_py/prac_70.py
+++ Question_61_70/practices_py/prac_70.py
@@ -4,24 +4,21 @@ import numpy as np
 # Read image
 img = cv2.imread("Question_61_70\imori.jpg")
 
-# alpha blend
-def track(img):
-  hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-  H = hsv[..., 0]
-  # hsvのhの値が180<=h<=260となる部分を抽出して、RGBに直すときにその抽出した部分のみ255にする。
-  mask = np.zeros_like(H)
-  mask[(H >= 180) & (H <= 260)] = 255
-  rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-  gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
+# make mask
+def get_mask(hsv): 
+  mask = np.zeros_like(hsv[..., 0]) 
+  mask[np.logical_and((hsv[..., 0] >= 90), (hsv[..., 0] <= 150))] = 255 
+  return mask
 
-  gray[mask == 255] = 255
-  gray[mask == 0] = 0
-  out = gray.astype(np.uint8) / 255.
-  
-  return out
+# RGB > HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-out = track(img)
+# color tracking
+mask = get_mask(hsv)
+
+out = mask.astype(np.uint8)
 
 # Save result
 cv2.imshow("result", out)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
