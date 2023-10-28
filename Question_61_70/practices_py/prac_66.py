742cdb999d4c078c667db18af5890e759b2ce6bb
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-28 16:54:43 +0900

    add: prac66

--- Question_61_70/practices_py/prac_66.py
+++ Question_61_70/practices_py/prac_66.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 img = cv2.imread('Question_41_50\imori.jpg')
 
@@ -30,6 +30,24 @@ def hog(img):
     
     return mag, ang_quantized
 
+
+# get gradient histogram
+def gradient_histogram(gradient_quantized, magnitude, N=8):
+    # get shape
+    H, W = magnitude.shape
+    # get cell num
+    cell_N_H = H // N
+    cell_N_W = W // N
+    histogram = np.zeros((cell_N_H, cell_N_W, 9), dtype=np.float32)
+    # each pixel
+    for y in range(cell_N_H):
+        for x in range(cell_N_W):
+            for j in range(N):
+                for i in range(N):
+                    histogram[y, x, gradient_quantized[y * 4 + j, x * 4 + i]] += magnitude[y * 4 + j, x * 4 + i]
+                    
+    return histogram
+
 mag , ang_quantized = hog(img)
 
 # Write gradient magnitude to file
@@ -49,7 +67,19 @@ C = [[255, 0, 0], [0, 255, 0], [0, 0, 255], [255, 255, 0], [255, 0, 255], [0, 25
 for i in range(9):
     out[ang_quantized == i] = C[i]
 
-    
+# 4. Gradient histogram
+histogram = gradient_histogram(ang_quantized, mag)
+
+
+# write histogram to file
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    plt.imshow(histogram[..., i])
+    plt.axis('off')
+    plt.xticks(color="None")
+    plt.yticks(color="None")
+
+plt.show()
 #result
 cv2.imshow('result', out)
 cv2.waitKey(0)
