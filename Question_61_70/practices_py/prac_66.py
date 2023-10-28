8d4e0d0d2636b7d2a62f1aa235c6723334ac1450
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-28 16:17:12 +0900

    add: prac66変更

--- Question_61_70/practices_py/prac_66.py
+++ Question_61_70/practices_py/prac_66.py
@@ -2,42 +2,27 @@ import cv2
 import numpy as np
 # from matplotlib import pyplot as plt
 
-img = cv2.imread('Question_41_50\\thorino.jpg')
+img = cv2.imread('Question_41_50\imori.jpg')
 
-def canny(img):
-      # 画像の高さ、幅、色を取得
-    dst = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    out = cv2.Canny(dst, 120,120)
-    return out    
-
-#result_img
-# cv2.imwrite('answers_image/answer4.jpg',img2)
-edges = canny(img)
-# 検出された直線の一覧の形状がndarrayで返される
-lines =cv2.HoughLines(edges, rho=1, theta=np.pi/180, threshold=75)
-
-def draw_line(img, theta, rho):
-    h, w = img.shape[:2]
-    if np.isclose(np.sin(theta), 0):
-        x1, y1 = rho, 0
-        x2, y2 = rho, h
-    else:
-        calc_y = lambda x: rho / np.sin(theta) - x * np.cos(theta) / np.sin(theta)
-        x1, y1 = 0, calc_y(0)
-        x2, y2 = w, calc_y(w)
-
-    # float -> int
-    x1, y1, x2, y2 = list(map(int, [x1, y1, x2, y2]))
-
-    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-
-# 直線を描画する。
-if lines is not None:
-    for rho, theta in lines.squeeze(axis=1):
-        draw_line(img, theta, rho)
-        
-cv2.imshow('result', img)
+def hog(img):
+    H,W,C = img.shape
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_pad = np.pad(gray,(1,1),'edge')
+    
+    gx = img_pad[1:H+1,2:] - img_pad[1:H+1, :W]
+    gy = img_pad[2:,1:W+1] - img_pad[:H, 1:W+1]
+    gx[gx == 0] = 1e-6
+    
+    mag = np.sqrt(gx**2 + gy**2) / 255.
+    ang = np.arctan(gy / gx)
+    
+    # quantization 量子化する
+    ang_quantized = np.zeros_like(ang, dtype=np.int)
+    
+    
+    
+#result
+cv2.imshow('result', )
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
--- Question_61_70/practices_py/prac_70.py
+++ Question_61_70/practices_py/prac_70.py
@@ -2,17 +2,25 @@ import cv2
 import numpy as np
 
 # Read image
-img1 = cv2.imread("Question_51_60\imori.jpg").astype(np.float32)
-img2 = cv2.imread("Question_51_60\\thorino.jpg").astype(np.float32)
+img = cv2.imread("Question_61_70\imori.jpg")
 
 # alpha blend
-def alpha_blend(img1, img2, alpha):
-	# blend
-	out = img1 * alpha + img2 * (1 - alpha)
-	out = out.astype(np.uint8)
-	return out
+def track(img):
+  hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+  H = hsv[..., 0]
+  # hsvのhの値が180<=h<=260となる部分を抽出して、RGBに直すときにその抽出した部分のみ255にする。
+  mask = np.zeros_like(H)
+  mask[(H >= 180) & (H <= 260)] = 255
+  rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+  gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
 
-out = alpha_blend(img1, img2, 0.6)
+  gray[mask == 255] = 255
+  gray[mask == 0] = 0
+  out = gray.astype(np.uint8) / 255.
+  
+  return out
+
+out = track(img)
 
 # Save result
 cv2.imshow("result", out)
