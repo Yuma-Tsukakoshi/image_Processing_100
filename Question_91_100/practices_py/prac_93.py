9065b52ce3447f29baf553ad1778801286fb4800
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-08 20:19:27 +0900

    add:prac94

--- Question_91_100/practices_py/prac_93.py
+++ Question_91_100/practices_py/prac_93.py
@@ -1,8 +1,10 @@
 import numpy as np
+import cv2
 
 # [x1, y1, x2, y2] x1,y1...矩形の左上のx,y  x2,y2...矩形の右下のx,y
-a = np.array((50, 50, 150, 150), dtype=np.float32)
-b = np.array((60, 60, 170, 160), dtype=np.float32)
+img = cv2.imread("Question_91_100/imori.jpg").astype(np.float32)
+a = (50, 50, 150, 150)
+b = (60, 60, 170, 160)
 
 def iou(a,b):
   R1 = (a[2] - a[0]) * (a[3] - a[1])
@@ -10,8 +12,10 @@ def iou(a,b):
   RoI_array = np.array((max(a[0], b[0]), max(a[1], b[1]), min(a[2], b[2]), min(a[3], b[3])), dtype=np.float32)
   RoI = (RoI_array[2] - RoI_array[0]) * (RoI_array[3] - RoI_array[1])
   iou = RoI / (R1 + R2 -RoI)
-  
-  return iou
+  cv2.rectangle(img,(a[0],a[1]),(a[2],a[3]),(255,0,0),1)
+  cv2.rectangle(img,(b[0],b[1]),(b[2],b[3]),(0,255,0),1)
+  return img
 
-result = iou(a,b)
-print(result)
+out  = iou(a,b)
+cv2.imshow('result',out)
+cv2.waitKey(0)
--- Question_91_100/practices_py/prac_94.py
+++ Question_91_100/practices_py/prac_94.py
@@ -1,51 +1,41 @@
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from glob import glob
+import cv2
 
-## Dicrease color
-def dic_color(img):
-    img //= 63
-    img = img * 64 + 32
+
+np.random.seed(0)
+img = cv2.imread("Question_91_100\imori_1.jpg")
+H,W,C = img.shape
+
+gt = (47, 41, 129, 103)
+def iou(a,b):
+    R1 = (a[2] - a[0]) * (a[3] - a[1])
+    R2 = (b[2] - b[0]) * (b[3] - b[1])
+    RoI_array = np.array((max(a[0], b[0]), max(a[1], b[1]), min(a[2], b[2]), min(a[3], b[3])), dtype=np.float32)
+    RoI = (RoI_array[2] - RoI_array[0]) * (RoI_array[3] - RoI_array[1])
+    iou = RoI / (R1 + R2 -RoI)
+    
+    return iou
+
+def make_rectangle(gt,img):
+    
+    for i in range(200):
+        x1 = np.random.randint(W-60)
+        y1 = np.random.randint(H-60)
+        x2 = x1 + 60
+        y2 = y1 + 60
+        
+        a = (x1,y1,x2,y2)
+        
+        result = iou(a,gt)
+        if result >= 0.5:
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,255),1)
+        else:
+            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0),1)
+            
+    cv2.rectangle(img,(gt[0],gt[1]),(gt[2],gt[3]),(0,255,0),1)
     return img
 
-## Database
-def get_DB():
-    # get image paths
-    train = glob("dataset/train_*")
-    train.sort()
-    print(train)
+out = make_rectangle(gt,img)
 
-    # prepare database
-    db = np.zeros((len(train), 13), dtype=np.int32)
-
-    # each image
-    for i, path in enumerate(train):
-        img = dic_color(cv2.imread(path))
-        # get histogram
-        for j in range(4):
-            db[i, j] = len(np.where(img[..., 0] == (64 * j + 32))[0])
-            db[i, j+4] = len(np.where(img[..., 1] == (64 * j + 32))[0])
-            db[i, j+8] = len(np.where(img[..., 2] == (64 * j + 32))[0])
-
-        # get class
-        if 'akahara' in path:
-            cls = 0
-        elif 'madara' in path:
-            cls = 1
-
-        # store class label
-        db[i, -1] = cls
-
-        img_h = img.copy() // 64
-        img_h[..., 1] += 4
-        img_h[..., 2] += 8
-        plt.subplot(2, 5, i+1)
-        plt.hist(img_h.ravel(), bins=12, rwidth=0.8)
-        plt.title(path)
-
-    print(db)
-    plt.show()
-
-# get database
-get_DB()
+cv2.imshow('result',out)
+cv2.waitKey(0)
